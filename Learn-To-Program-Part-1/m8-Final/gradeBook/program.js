f8f593be29d2a89f7920dedc658603c34dfe36f5
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2016-02-20 00:57:44 -0500

    added localhost string output for average and lettergrade using querystring with grades

--- Learn-To-Program-Part-1/m8-Final/gradeBook/program.js
+++ Learn-To-Program-Part-1/m8-Final/gradeBook/program.js
@@ -4,6 +4,26 @@ var book = require("./lib/grades").book;
 var express = require("express");
 var app = express();
 
+// have app respond to GET request, / is root of site
+app.get("/", function(req, res){
+    res.send("Hello World");
+});
+
+app.get("/grade", function(req, res){
+    
+    // "1,2,3".split(",") => [1,2,3]
+    var grades = req.query.grades.split(",");
+    for ( var i=0; i < grades.length; i++){
+        book.addGrade(parseInt(grades[i]));
+    }
+    
+    var average = book.getAverage();
+    var letter = book.getLetterGrade();
+    
+    res.send("Your average is " + average + " grade " + letter);
+    
+})
+
 // have app listen on port 3000
 app.listen(3000);
 console.log("server ready...");
