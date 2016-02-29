6596437126a06edc3dba962f17a1e2a44fee3b9e
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2016-02-29 13:31:51 -0500

    added underscore.js library with array examples

--- JavaScript-Fundamentals/m5-Types-and-Libraries/Scripts/array.js
+++ JavaScript-Fundamentals/m5-Types-and-Libraries/Scripts/array.js
@@ -3,10 +3,20 @@ function write(message) {
   document.getElementById('message').innerHTML += message + '<br/>';
 }
 
-
 function newParagraph(){
     write("")
 }
+
+// update second p element 
+function writeP2(message_2) {
+  document.getElementById('message_2').innerHTML += message_2 + '<br/>';
+}
+
+function newP2(){
+    writeP2("")
+}
+
+
 
 // empty erray
 var empty = [];
@@ -51,3 +61,58 @@ write("[2,1,8,99,75,10,22].sort(function (first,second){ "
       + [2,1,8,99,75,10,22].sort(function (first,second){ 
         return first - second;
        }));
+
+/*
+* Array demo using Underscore.js
+*/
+
+var numbers = [1, 2, 3];
+
+// functional style - loop through array then output
+_.each(numbers, function(num){
+       writeP2(num);
+});
+newP2();
+
+
+// Object-Oriented Style - wrap the numbers array then output
+_(numbers).each(function(num){
+    writeP2(num);
+});
+newP2();
+
+// map - translates collection to another
+var doubled = _(numbers).map(function(num){
+    return num * 2;
+});
+writeP2("[1, 2, 3] doubled: " + doubled);
+newP2();
+
+// reduce - ditills collection to a single val
+var total = _(doubled).reduce(function(memo, num){
+    return memo += num;
+}, 0);
+writeP2("[2, 4, 6] reduced: " + total);
+newP2();
+
+// select - used to filter a collection via callback function
+var evens = _([1,2,3,4,5]).select(function(num){
+    return num % 2 === 0;
+});
+writeP2("[1,2,3,4,5] evens: " + evens);
+newP2();
+
+// all - returns true if callback func returns true for all items in array
+var allNumbers = _([1,2,'a']).all(function(item){
+    return typeof item === "number";
+});
+writeP2(allNumbers);
+newP2();
+
+// include - simple test if collection contains an item - return boolean
+var hasPeanuts = _(['wheat flour', 'sugar', 'salt', 'peanut', 'caramel']).include("peanut");
+writeP2(hasPeanuts);
+newP2();
+
+// max
+writeP2(_({a:3, b:2, c:1}).max());
