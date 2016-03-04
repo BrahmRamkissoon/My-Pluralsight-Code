fa56e651fa54e2bb334738481b6e4c5fbfdf7873
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2016-03-04 17:16:18 -0500

    added revealing module pattern demo

--- Structuring-JavaScript-Code/m4-Revealing-Module/Scripts/revealing-module.js
+++ Structuring-JavaScript-Code/m4-Revealing-Module/Scripts/revealing-module.js
@@ -1,21 +1,30 @@
 // normally use jQuery for window.onload
 window.onload = function(){
-    var calc = new Calculator('Output');
-    calc.add(2, 2);
-    alert(calc.eqCtl.innerHTML);
+    calculator.add(2,2);
+    calculator.subtract(2,2);
 };
 
 // calculator.js
 // Constructor
-var Calculator = function(eq){
+var calculator = function(eq){
     // private members
     var eqCtl = document.getElementById(eq);
     
-    return{
-        // public
-        add: function(x,y){
-            var val = x + y;
-            eqCtl.innerHTML = val;
-        }  
+    add = function(x,y){
+        var val = x + y;
+        eqCtl.innerHTML = val;
+    },
+    
+    subtract = function(x, y){
+        alert( x - y);
     };
-};
+        
+    
+    // public members
+    return{
+        add: add,
+        subtract: subtract
+    };
+}('Output');
+
+// self - invoking
